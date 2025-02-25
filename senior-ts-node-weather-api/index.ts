c736508b216fdfe7b0bf7fd7fd0cff489f0edeae
Ben Brachmann <49547103+bevenio@users.noreply.github.com>
2025-02-25 12:17:38 +0100

    Update index.ts

--- senior-ts-node-weather-api/index.ts
+++ senior-ts-node-weather-api/index.ts
@@ -1,9 +1,24 @@
-import { ApiRepository } from './api.repository'
+import { ApiRepository } from "./api.repository";
 
-const apiRepository = new ApiRepository()
+const apiRepository = new ApiRepository();
 
-apiRepository.getWeatherFromServiceA(51.5074, 0.1278)
-    .then(data => console.log(data))
+const locations = [
+  { name: "Zürich", lat: 47.36667, long: 8.55 },
+  { name: "Luzern", lat: 47.05048, long: 8.30635 },
+  { name: "Bern", lat: 46.94809, long: 7.44744 },
+  { name: "Basel", lat: 47.55839, long: 7.57327 },
+  { name: "Genf", lat: 46.20222, long: 6.14569 },
+  { name: "Lausanne", lat: 46.516, long: 6.63282 },
+  { name: "St. Gallen", lat: 47.42391, long: 9.37477 },
+  { name: "Lugano", lat: 46.01008, long: 8.96004 },
+  { name: "Winterthur", lat: 47.5, long: 8.75 },
+  { name: "Thun", lat: 46.75118, long: 7.62166 },
+];
 
-apiRepository.getWeatherFromServiceB(51.5074, 0.1278)
-    .then(data => console.log(data))
+apiRepository
+  .getWeatherFromServiceA(locations[0].lat, locations[0].long)
+  .then((data) => console.log(data));
+
+apiRepository
+  .getWeatherFromServiceB(locations[1].lat, locations[1].long)
+  .then((data) => console.log(data));
